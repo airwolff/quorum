337f6c96a192c9e3e7f5e4da6aa1fa9ba0a0bb08
Andrew Wolff <andy.wolff@gmail.com>
2017-02-02 13:01:12 -0600

    fixing db

--- server/modules/decoder.js
+++ server/modules/decoder.js
@@ -3,9 +3,7 @@ var pg = require('pg');
 var admin = require("firebase-admin");
 var config = require('../config/dbconfig');
 
-var pool = new pg.Pool({
-	database: config.database
-});
+var pool = new pg.Pool(config.pg);
 
 
 admin.initializeApp({
