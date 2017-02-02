89b7492c6454d1343db2d0b83d6483afd3ffab5f
Andrew Wolff <andy.wolff@gmail.com>
2017-02-02 10:54:42 -0600

    dotenv

--- server/config/dbconfig.js
+++ server/config/dbconfig.js
@@ -1,5 +1,11 @@
 var config = {
-	database: 'quorum'
+  user: process.env.DATA_BASE_USER,
+  database: process.env.DATA_BASE,
+  password: process.env.DATA_BASE_PASSWORD, 
+  host: process.env.DATA_BASE_HOST, 
+  ssl: true,
+  max: 10, // max number of clients in the pool
+  idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
 };
 
 module.exports = config;
